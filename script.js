10121883442409e1ddb9f1e56aa6410b5f7fd0fb
somildholakia <somildholakia0852@gmail.com>
2025-12-26 20:07:45 +0530

    updating

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 
 let count = 0;
-let count_el = document.getElementById("count")
+let lr_count = document.getElementById("hh");
+let save_number = 0;
+let pre = document.getElementById("pre");
 
 function Increment(){
-    count = count + 1
-    count_el.innerText = count
-    console.log(count) 
+    count += 1;
+    lr_count.innerText = count;
+    console.log(count);
+}
+
+function save(){
+    save_number = count;
+    console.log("save number is:",save_number);
+    count = 0;
+    lr_count.innerText = 0;
+    pre.innerText = ("-" + save_number);
 
 }
+
+
+let namee = "somil";
+let greeting = "Hi,my name is ";
+let myGreeting = (greeting + namee);
+
+console.log(myGreeting);
